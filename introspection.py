0e51abad58fcde604425952a2911a855945357b1
Evgenya888 <e.v.a240967@gmail.com>
2025-01-14 14:13:03 +0700

    Add files via upload

--- introspection.py
+++ introspection.py
@@ -1,21 +1,16 @@
+from pprint import pprint
+
 def introspection_info(obj):
-
-    obj_type = type(obj).__name__
-
-    attributes = dir(obj)
-
-    methods = [method for method in attributes if callable(getattr(obj, method))]
-
-    module = obj.__class__.__module__
-
-    info = {'type': obj_type, 'attributes': attributes, 'methods': methods, 'module': module},
-
-    return info
+    object_type = type(obj)
+    object_attributes =  [attr for attr in dir(obj) if not callable(getattr(obj, attr)) and not attr.startswith('__')]
+    object_methods = [method for method in dir(obj) if callable(getattr(obj, method))]
+    object_module = obj.__class__.__module__ if hasattr(obj, '__class__') else 'built-in'
+    return {
+        'type': object_type,
+        'attributes': object_attributes,
+        'methods': object_methods,
+        'module': object_module,
+    }
 
 number_info = introspection_info(42)
-print(number_info)
-
-string_info = introspection_info('Hello')
-print(string_info)
-
-list_info = introspection_info([1, 20, 4.0, 'word'])
+pprint(number_info)
