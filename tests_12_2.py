86bbad080b29adb13278d71454d740548c0b5475
Evgenya888 <e.v.a240967@gmail.com>
2025-01-22 08:21:53 +0700

    Add files via upload

--- tests_12_2.py
+++ tests_12_2.py
@@ -1,5 +1,45 @@
 import unittest
-from runner import Runner
+import inspect
+from runner_and_tournament import Runner, Tournament
 
 
-class RunnerTest(unittest.TestCase):
+class TournamentTest(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.all_results = {}
+
+    def setUp(self):
+        self.usain = Runner('Усэйн', 10)
+        self.andrey = Runner('Андрей', 9)
+        self.nik = Runner('Ник', 3)
+
+    @classmethod
+    def tearDownClass(cls):
+        print()
+        for test in cls.all_results:
+            print()
+            print(f'{test}:')
+            print({k: str(v) for k, v in cls.all_results[test].items()})
+
+    def test_usain_nik(self):
+        tour = Tournament(90, self.usain, self.nik)
+        results = tour.start()
+        self.all_results[inspect.stack()[0][3]] = results
+        self.assertTrue('Ник' == results[len(results)].name)
+
+    def test_andrey_nik(self):
+        tour = Tournament(90, self.andrey, self.nik)
+        results = tour.start()
+        self.all_results[inspect.stack()[0][3]] = results
+        self.assertTrue('Ник' == results[len(results)].name)
+
+    def test_usain_andrey_nik(self):
+        tour = Tournament(90, self.usain, self.andrey, self.nik)
+        results = tour.start()
+        self.all_results[inspect.stack()[0][3]] = results
+        self.assertTrue('Ник' == results[len(results)].name)
+
+
+
+if __name__ == '__main__':
+    unittest.main()
